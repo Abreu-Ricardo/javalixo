f0a65cd434c0750674a36bc0fb4cdbd8ecae3a91
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2020-02-19 07:41:11 -0400

    Add files via upload

---
+++ Paunocujava.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Paunocujava{
+    public static void main(String[] args){
+        int x;
+        int j;
+        int num;
+
+        Scanner n = new Scanner(System.in);
+        int s = n.nextInt();
+        n.close();
+
+        for(int i = 1; i <= s; i++){
+            x = i;
+
+            for (j = 1; j <= i; j++){
+                
+                num = x*j;
+                System.out.printf(num + " ");
+            }
+            
+            x++;
+            System.out.println();
+        }
+    }
+}
