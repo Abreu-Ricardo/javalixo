e4075788be2e19eba6a49cdbc260830eef3a1c3a
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2020-04-07 15:33:23 -0400

    Update Empregado.java

--- Empregado.java
+++ Empregado.java
@@ -7,7 +7,7 @@ public class Empregado{
 
         Scanner entrada1 = new Scanner(System.in);
         
-        //ComissionEmployee empragado1 = new ComissionEmployee("Xablau", "Cablau", "SOUGAY", 1346.12, 0.12);
+        //ComissionEmployee empragado1 = new ComissionEmployee("Xablau", "Cablau", "ABRACADABRA", 1346.12, 0.12);
         //ComissionEmployee empragado1 = new ComissionEmployee(entrada1.nextLine(), entrada1.nextLine(), entrada1.nextLine(), entrada1.nextDouble(), entrada1.nextDouble());
         BasePlusComissionEmployee empregado1 = new BasePlusComissionEmployee(entrada1.nextLine(), entrada1.nextLine(), entrada1.nextLine(), entrada1.nextDouble(), entrada1.nextDouble());
         empregado1.setbaseSalary(entrada1.nextDouble());
